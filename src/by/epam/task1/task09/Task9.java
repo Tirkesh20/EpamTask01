7b3283392d767804f232538b05d504708d561c1c
tirkesh_yagshymyradov <tirkeshy@gmail.com>
2021-05-22 17:51:06 +0300

    Task 9 done and optimized

---
+++ src/by/epam/task1/task09/Task9.java
@@ -0,0 +1,28 @@
+package by.epam.task1.task09;
+
+import by.epam.task1.properties.Scanners;
+
+/*
+    Вычислить длину окружности и площадь круга одного и того же заданного радиуса R
+ */
+    public class Task9 {
+
+    public static void findSAndP(){
+
+        System.out.println("enter r");
+        double r = Scanners.doubleScanner();
+        double s = findArea(r);
+        double p = findCircle(r);
+        System.out.printf("Area=%d Circle=%d",s,p);
+
+    }
+
+    public static double findArea(double r){
+        return Math.PI*Math.pow(r,2);
+    }
+
+    public static double findCircle(double r){
+        return 2*Math.PI*r;
+    }
+
+}
---
+++ src/by/epam/task1/task09/Test9.java
@@ -0,0 +1,8 @@
+package by.epam.task1.task09;
+
+public class Test9 {
+
+    public static void main(String[] args) {
+        Task9.findSAndP();
+    }
+}
